e09be6c86f639309ccd138ea708c5629eb0fe63b
mako116 <nwakwueemmanuel1@gmail.com>
2025-09-11 19:18:56 +0100

    m

--- src/admin/features/Users/Organization.jsx
+++ src/admin/features/Users/Organization.jsx
@@ -10,6 +10,7 @@ import { CreateOrganization } from "./modal/CreateOrganization";
 import axiosClient from "../../../axiosClient";
 import { useRequest } from "../../../hooks/useRequest";
 import { toast } from "react-toastify";
+import { FiUserPlus } from "react-icons/fi";
 
 export const Organization = () => {
   const [pageSize, setPageSize] = useState(4);
@@ -17,38 +18,76 @@ export const Organization = () => {
   const [rowActions, setRowActions] = useState({});
   const navigate = useNavigate();
   const [organizations, setOrganizations] = useState([]);
-  const {makeRequest} = useRequest()
-  const [refresh,setRefresh] = useState(false);
-  useEffect(()=>{
-    const getOrgs = async()=>{
-      const res = await axiosClient.get('/get-organizations');
-      
+  const { makeRequest } = useRequest();
+  const [refresh, setRefresh] = useState(false);
+  useEffect(() => {
+    const getOrgs = async () => {
+      const res = await axiosClient.get("/get-organizations");
+
       setOrganizations(res.data.organizations);
-    }
-    getOrgs()
-  },[refresh])
-const handleDelete = async(id)=>{
-  const res = await makeRequest('/remove-organization',{approved:false, orgId:id});
-  if(res.error) return;
-  toast.success('Organization Removed successfully');
-  handleSelect(id, "Decline", "red.500", <MdOutlineCancel boxSize={3} />)
-  setRefresh(prev=>!prev);
-}
-   const [isOpen, setIsOpen] = useState(false);
-    
-        const handleAddUser = () => {
-           setIsOpen(true);
-         };
-          const handleClose = () => {
-          setIsOpen(false);
-          };
+    };
+    getOrgs();
+  }, [refresh]);
+  const handleDelete = async (id) => {
+    const res = await makeRequest("/remove-organization", {
+      approved: false,
+      orgId: id,
+    });
+    if (res.error) return;
+    toast.success("Organization Removed successfully");
+    handleSelect(id, "Decline", "red.500", <MdOutlineCancel boxSize={3} />);
+    setRefresh((prev) => !prev);
+  };
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleAddUser = () => {
+    setIsOpen(true);
+  };
+  const handleClose = () => {
+    setIsOpen(false);
+  };
 
   const tableData = [
-    { UserId: "#38734", Name: "Jamal", image: img, Profession: "Web Developer", Experience: "3", Timestamp: "09/08/24, 12:00pm" },
-    { UserId: "#12233", Name: "Lydia", image: img, Profession: "UI Designer", Experience: "5", Timestamp: "09/08/24, 12:10pm" },
-    { UserId: "#12234", Name: "Alice", image: img, Profession: "Backend Dev", Experience: "4", Timestamp: "09/08/24, 12:20pm" },
-    { UserId: "#12235", Name: "Bob", image: img, Profession: "Frontend Dev", Experience: "2", Timestamp: "09/08/24, 12:30pm" },
-    { UserId: "#12236", Name: "Charlie", image: img, Profession: "UI Designer", Experience: "5", Timestamp: "09/08/24, 12:40pm" },
+    {
+      UserId: "#38734",
+      Name: "Jamal",
+      image: img,
+      Profession: "Web Developer",
+      Experience: "3",
+      Timestamp: "09/08/24, 12:00pm",
+    },
+    {
+      UserId: "#12233",
+      Name: "Lydia",
+      image: img,
+      Profession: "UI Designer",
+      Experience: "5",
+      Timestamp: "09/08/24, 12:10pm",
+    },
+    {
+      UserId: "#12234",
+      Name: "Alice",
+      image: img,
+      Profession: "Backend Dev",
+      Experience: "4",
+      Timestamp: "09/08/24, 12:20pm",
+    },
+    {
+      UserId: "#12235",
+      Name: "Bob",
+      image: img,
+      Profession: "Frontend Dev",
+      Experience: "2",
+      Timestamp: "09/08/24, 12:30pm",
+    },
+    {
+      UserId: "#12236",
+      Name: "Charlie",
+      image: img,
+      Profession: "UI Designer",
+      Experience: "5",
+      Timestamp: "09/08/24, 12:40pm",
+    },
   ];
 
   const handleSelect = (userId, label, color, icon = null) => {
@@ -64,106 +103,118 @@ const handleDelete = async(id)=>{
       col_5: { col_5_1: "Members" },
       col_6: { col_6_1: "Action" },
     },
-    row: organizations.length>0? organizations.map((row, index) => {
-      const selected = rowActions[row.id] || { label: "Action", color: "gray.600", icon: null };
-      const uniqueKey = `${row.id}-${index}`;
-      return {
-        row_0: uniqueKey,
-        row_1: { row_1_1: row.id },
-        row_2: { row_2_1: row.logo, row_2_2: row.name },
-        row_3: { row_3_1: row.location },
-        row_4: { row_4_1: (
-          <Text textDecoration={'underline'}>
-            {row.website_url}
-          </Text>
-        ) },
-        row_5: { row_5_1: row.users?.length },
-        row_6: {
-          row_6_1: (
-            <Menu.Root key={uniqueKey}>
-              <Menu.Trigger asChild>
-                <Button
-                  size="sm"
-                  border={`1px solid ${selected.color}`}
-                  rounded={20}
-                  variant="outline"
-                  color={selected.color}
-                >
-                  <HStack spacing={1}>
-                    {selected.icon && selected.icon}
-                    <Text fontSize="13px" fontWeight="400" fontFamily="OutfitRegular">{selected.label}</Text>
-                    {!selected.icon && <IoIosArrowDown />}
-                  </HStack>
-                </Button>
-              </Menu.Trigger>
-              <Portal>
-                <Menu.Positioner>
-                  <Menu.Content cursor="pointer" rounded={20}>
-                    {/* <Menu.Item
+    row:
+      organizations.length > 0 ? (
+        organizations.map((row, index) => {
+          const selected = rowActions[row.id] || {
+            label: "Action",
+            color: "gray.600",
+            icon: null,
+          };
+          const uniqueKey = `${row.id}-${index}`;
+          return {
+            row_0: uniqueKey,
+            row_1: { row_1_1: row.id },
+            row_2: { row_2_1: row.logo, row_2_2: row.name },
+            row_3: { row_3_1: row.location },
+            row_4: {
+              row_4_1: (
+                <Text textDecoration={"underline"}>{row.website_url}</Text>
+              ),
+            },
+            row_5: { row_5_1: row.users?.length },
+            row_6: {
+              row_6_1: (
+                <Menu.Root key={uniqueKey}>
+                  <Menu.Trigger asChild>
+                    <Button
+                      size="sm"
+                      border={`1px solid ${selected.color}`}
+                      rounded={20}
+                      variant="outline"
+                      color={selected.color}
+                    >
+                      <HStack spacing={1}>
+                        {selected.icon && selected.icon}
+                        <Text
+                          fontSize="13px"
+                          fontWeight="400"
+                          fontFamily="OutfitRegular"
+                        >
+                          {selected.label}
+                        </Text>
+                        {!selected.icon && <IoIosArrowDown />}
+                      </HStack>
+                    </Button>
+                  </Menu.Trigger>
+                  <Portal>
+                    <Menu.Positioner>
+                      <Menu.Content cursor="pointer" rounded={20}>
+                        {/* <Menu.Item
                       color="#333333CC"
                       onClick={() => handleSelect(row.UserId, "Approve", "green.500", <IoMdCheckboxOutline boxSize={3} />)}
                     > 
                       <IoMdCheckboxOutline /> Remove
                     </Menu.Item> */}
-                    {/* <Menu.Item color="#333333CC" onClick={() => navigate(`/admin/organization-details`)}>View Details</Menu.Item> */}
-                    <Menu.Item
-                      color="#333333CC"
-                      onClick={() => handleDelete(row.id)}
-                    >
-                      <MdOutlineCancel /> Remove
-                    </Menu.Item>
-                  </Menu.Content>
-                </Menu.Positioner>
-              </Portal>
-            </Menu.Root>
-          ),
-        },
-      };
-    }):<Text>No Organizations Yet</Text>,
+                        {/* <Menu.Item color="#333333CC" onClick={() => navigate(`/admin/organization-details`)}>View Details</Menu.Item> */}
+                        <Menu.Item
+                          color="#333333CC"
+                          onClick={() => handleDelete(row.id)}
+                        >
+                          <MdOutlineCancel /> Remove
+                        </Menu.Item>
+                      </Menu.Content>
+                    </Menu.Positioner>
+                  </Portal>
+                </Menu.Root>
+              ),
+            },
+          };
+        })
+      ) : (
+        <Text>No Organizations Yet</Text>
+      ),
   };
 
   return (
-    <Box   bg="#F5F6FA" py={{base:1,md:6}}>
-     
-       <Button
-           mb={4}
-           position={'absolute'}
-           right={0}
-           top={5}
-             size="sm"
-             border={`1px solid #333`}
-              rounded={20}
-              color={"#333"}
-              bg="#fff"
-              onClick={handleAddUser}
-             _hover={{ bg: "#f0f0f0" }}
-             >
-            <HStack spacing={2}>
-             <FaUserPlus size={12} />
-              <Text
-               fontSize={{base:10,md:13}}
-               fontWeight="400"
-               fontFamily="OutfitRegular"
-               >
-                Add New Organization
-              </Text>
-             </HStack>
-          </Button>
-     
-         <CreateOrganization 
-           isOpen={isOpen}
-           onClose={handleClose}
-         />
-         {
-          organizations.length>0 ?<BottomTable
-        dataTable={dataTable}
-        pageSize={pageSize}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-        setPageSize={setPageSize}
-      />:<Text>No organizations yet</Text>
-         }
-     
+    <Box bg="#F5F6FA" py={{ base: 1, md: 6 }}>
+      <Button
+        mb={4}
+        position={"absolute"}
+        right={0}
+        top={5}
+        size="sm"
+        border={`1px solid #333`}
+        rounded={20}
+        color={"#333"}
+        bg="#fff"
+        onClick={handleAddUser}
+        _hover={{ bg: "#f0f0f0" }}
+      >
+        <HStack spacing={2}>
+          <FiUserPlus size={12} />
+          <Text
+            fontSize={{ base: 10, md: 13 }}
+            fontWeight="400"
+            fontFamily="OutfitRegular"
+          >
+            Add New Organization
+          </Text>
+        </HStack>
+      </Button>
+
+      <CreateOrganization isOpen={isOpen} onClose={handleClose} />
+      {organizations.length > 0 ? (
+        <BottomTable
+          dataTable={dataTable}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          setPageSize={setPageSize}
+        />
+      ) : (
+        <Text>No organizations yet</Text>
+      )}
     </Box>
   );
 };
--- src/admin/features/mentoring/Mentoring.jsx
+++ src/admin/features/mentoring/Mentoring.jsx
@@ -14,7 +14,7 @@ import {
   SimpleGrid,
   Text,
 } from "@chakra-ui/react";
-import  { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { cardData } from "../../../hooks/useData";
 import { MentoringDetails } from "./MentoringDetails";
 import { MdKeyboardArrowRight } from "react-icons/md";
@@ -33,23 +33,23 @@ export const AdminMentor = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [listings, setListings] = useState([]);
 
-  const {makeRequest} = useRequest();
+  const { makeRequest } = useRequest();
 
-  const handleDelete = async (id)=>{
-    const res = await makeRequest('/delete-listing', {listingId:id});
-    if(res.error) return;
-    toast.success('Listing deleted successfully');
-    setListings(prev=>prev.filter(item=>item.id!==id));
-  }
+  const handleDelete = async (id) => {
+    const res = await makeRequest("/delete-listing", { listingId: id });
+    if (res.error) return;
+    toast.success("Listing deleted successfully");
+    setListings((prev) => prev.filter((item) => item.id !== id));
+  };
 
-  useEffect(()=>{
-    const getListings = async()=>{
-      const res = await axiosClient.get('/get-all-listings');
-      
+  useEffect(() => {
+    const getListings = async () => {
+      const res = await axiosClient.get("/get-all-listings");
+
       setListings(res.data.listings);
-    }
+    };
     getListings();
-  }, [])
+  }, []);
 
   const handleCardClick = (card) => {
     setSelectedCard(card);
@@ -61,146 +61,155 @@ export const AdminMentor = () => {
     setSelectedCard(null);
   };
 
-    const frameworks = createListCollection({
+  const frameworks = createListCollection({
     items: [
       { label: "Experience", value: "Experience" },
       { label: "finances", value: "finances" },
       { label: "Angular", value: "angular" },
       { label: "Svelte", value: "svelte" },
     ],
-  })
+  });
 
-    const [isOpened, setIsOpened] = useState(false);
+  const [isOpened, setIsOpened] = useState(false);
 
-      const handleAction = () => {
-     setIsOpened(true);
+  const handleAction = () => {
+    setIsOpened(true);
   };
 
   const handleClosed = () => {
     setIsOpened(false);
-   };
+  };
   return (
-    <Box bg={'#F5F6FA'} h={"100%"} p={3}>
-      <Heading display={'flex'} pb={4} gap={2} alignItems={'center'}>
-         <IconButton
-         aria-label="Previous"
+    <Box bg={"#F5F6FA"} h={"100%"} p={3}>
+      <Heading display={"flex"} pb={4} gap={2} alignItems={"center"}>
+        <IconButton
+          aria-label="Previous"
           rounded="full"
-         bg="white"
-         border={'1px solid #9E9E9E'}
-         _hover={{ bg: "whiteAlpha.500" }}
-         size="sm"
-         >
+          bg="white"
+          border={"1px solid #9E9E9E"}
+          _hover={{ bg: "whiteAlpha.500" }}
+          size="sm"
+        >
           <IoIosArrowBack color="#9E9E9E" />
         </IconButton>
         Mentor Listings
       </Heading>
-        <Flex justifyContent={'space-between'} 
-        flexDirection={{base:'column',md:"row"}} 
-        pb={5} alignItems={{base:'flex-start',md:'center'}} 
-        gap={{base:4,md:10}}>
+      <Flex
+        justifyContent={"space-between"}
+        flexDirection={{ base: "column", md: "row" }}
+        pb={5}
+        alignItems={{ base: "flex-start", md: "center" }}
+        gap={{ base: 4, md: 10 }}
+      >
         <InputGroup
-         w={{base:'100%',md: 700}}
-         bg={'white'}
-           startElement={<CiSearch size={15} />}
-           >                
+          w={{ base: "100%", md: 700 }}
+          bg={"white"}
+          startElement={<CiSearch size={15} />}
+        >
           <Input
             py={6}
-          fontSize={10}
-          borderRadius={10}
-         placeholder="Search..."
-         /> 
-         </InputGroup>
+            fontSize={10}
+            borderRadius={10}
+            placeholder="Search..."
+          />
+        </InputGroup>
 
         <Dropdown
-        frameworks={frameworks}
-        // title={'finance'}
-        
+          frameworks={frameworks}
+          // title={'finance'}
         />
+      </Flex>
 
-        </Flex>
-        
       <SimpleGrid columns={{ base: 1, md: 2, xl: 3 }} spacing={6} gap={5}>
-        {listings.length>0? listings.map((card, idx) => (
-          <Card.Root
-            key={idx}
-            bg={"#fff"}
-            shadowColor={"#080F340F"}
-            shadow={"sm"}
-            rounded={20}
-            border={"1px solid #fff"}
-          >
-            <Card.Body gap="2">
-              <Avatar.Root mx={"auto"} boxSize={20} rounded={50}>
-                <Avatar.Image src={card.user.profile_picture||userAvatar} />
-                <Avatar.Fallback name={card.user.name} />
-              </Avatar.Root>
-               <Menu.Root >
-                      <Menu.Trigger 
-                         position={'absolute'}
-                         right={5}
-                          top={5}  asChild>
-                          <Button 
-                          p={0}
-                          rounded={30}
-                          bg={'#6C3433'}  size="sm">
-                           <BiDotsVerticalRounded size={10}/>
-                          </Button>
-                         </Menu.Trigger>
-                        <Portal>
-                         <Menu.Positioner>
-                           <Menu.Content>
-                           
-                            <Menu.Item value="new-file" onClick={()=>handleDelete(card.id)}>
-                              <BiTrash/>
-                              Delete
-                         </Menu.Item>
-                       </Menu.Content>
-                     </Menu.Positioner>
-                   </Portal>
+        {listings.length > 0 ? (
+          listings.map((card, idx) => (
+            <Card.Root
+              key={idx}
+              bg={"#fff"}
+              shadowColor={"#080F340F"}
+              shadow={"sm"}
+              rounded={20}
+              border={"1px solid #fff"}
+            >
+              <Card.Body gap="2">
+                <Avatar.Root mx={"auto"} boxSize={20} rounded={50}>
+                  <Avatar.Image src={card.user.profile_picture || userAvatar} />
+                  <Avatar.Fallback name={card.user.name} />
+                </Avatar.Root>
+                <Menu.Root>
+                  <Menu.Trigger position={"absolute"} right={5} top={5} asChild>
+                    <Button p={0} rounded={30} bg={"#fff"} size="sm">
+                      <BiDotsVerticalRounded size={10} color="#212121" />
+                    </Button>
+                  </Menu.Trigger>
+                  <Portal>
+                    <Menu.Positioner>
+                      <Menu.Content>
+                        <Menu.Item
+                          value="new-file"
+                          onClick={() => handleDelete(card.id)}
+                        >
+                          <BiTrash />
+                          Delete
+                        </Menu.Item>
+                      </Menu.Content>
+                    </Menu.Positioner>
+                  </Portal>
                 </Menu.Root>
-              <Text textAlign={"center"}
-              color={'#070416'}
-              fontSize={{base:12,md:16}}
-                fontFamily="InterRegular">
-                {card.user.first_name} {card.user.last_name}
-              </Text>
-              <Text textAlign={"center"}
-               color={'#64626D'}
-               fontSize={{base:12,md:16}}
-                fontFamily="LatoRegular"
+                <Text
+                  textAlign={"center"}
+                  color={"#070416"}
+                  fontSize={{ base: 12, md: 16 }}
+                  fontFamily="InterRegular"
                 >
-               {card.user.profession}
-              </Text>
-              <Card.Title
-                mt="2"
-                color={'#070416'}
-               fontSize={{base:12,md:16}}
-                textAlign={"center"}
-                fontFamily="InterBold"
-              >
-                {card.title}
-              </Card.Title>
-              <Button 
-              onClick={() => handleCardClick(card)} 
-              bg={'transparent'}>
-              <Card.Description 
-               py={1}
-               w={'100%'}
-              display={'flex'} 
-              alignItems={'center'} 
-              justifyContent={'space-between'} 
-              textAlign={"center"}>
-                <Text color={'#64626D'} 
-                fontSize={{base:12,md:16}}
-                fontFamily="InterRegular"
-                textAlign={"left"}>View Details</Text>
-                <MdKeyboardArrowRight />
-              </Card.Description>
-              </Button>
-            </Card.Body>
-             
-          </Card.Root>
-        )):<Text>No Listings available yet</Text>}
+                  {card.user.first_name} {card.user.last_name}
+                </Text>
+                <Text
+                  textAlign={"center"}
+                  color={"#64626D"}
+                  fontSize={{ base: 12, md: 16 }}
+                  fontFamily="LatoRegular"
+                >
+                  {card.user.profession}
+                </Text>
+                <Card.Title
+                  mt="2"
+                  color={"#070416"}
+                  fontSize={{ base: 12, md: 16 }}
+                  textAlign={"center"}
+                  fontFamily="InterBold"
+                >
+                  {card.title}
+                </Card.Title>
+                <Button
+                  onClick={() => handleCardClick(card)}
+                  bg={"transparent"}
+                >
+                  <Card.Description
+                    py={1}
+                    w={"100%"}
+                    display={"flex"}
+                    alignItems={"center"}
+                    justifyContent={"space-between"}
+                    textAlign={"center"}
+                  >
+                    <Text
+                      color={"#64626D"}
+                      fontSize={{ base: 12, md: 16 }}
+                      fontFamily="InterRegular"
+                      textAlign={"left"}
+                    >
+                      View Details
+                    </Text>
+                    <MdKeyboardArrowRight />
+                  </Card.Description>
+                </Button>
+              </Card.Body>
+            </Card.Root>
+          ))
+        ) : (
+          <Text>No Listings available yet</Text>
+        )}
       </SimpleGrid>
 
       {/* Modal */}
@@ -211,10 +220,7 @@ export const AdminMentor = () => {
           profile={selectedCard}
         />
       )}
-      <EditMentor 
-       isOpen={isOpened}
-          onClose={handleClosed}
-      />
+      <EditMentor isOpen={isOpened} onClose={handleClosed} />
     </Box>
   );
 };
