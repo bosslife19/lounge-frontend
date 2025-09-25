40fd7ee5bef6dde236a35535f8e3d601d3117c5a
bosslife19 <wokodavid001@gmail.com>
2025-09-25 13:50:21 +0100

    fixed more conflicts

--- src/user/features/directory/Directory.jsx
+++ src/user/features/directory/Directory.jsx
@@ -51,7 +51,6 @@ const Directory = () => {
   const [filteredResults, setFilteredResults] = useState([]);
 
   useEffect(() => {
-<<<<<<< HEAD
   if (search) {
     const lowerSearch = search.toLowerCase();
     const results = directoryData.filter((item) =>
@@ -78,33 +77,6 @@ const Directory = () => {
     setFilteredResults(directoryData); // if no search, show all
   }
 }, [locationSearch, directoryData]);
-=======
-    if (search) {
-      const lowerSearch = search.toLowerCase();
-      const results = directoryData.filter((item) =>
-        [item.first_name, item.last_name, item.name, item.organization.name]
-          .filter(Boolean) // removes null/undefined
-          .some((field) => field.toLowerCase().includes(lowerSearch))
-      );
-      setFilteredResults(results);
-    } else {
-      setFilteredResults(directoryData); // if no search, show all
-    }
-  }, [search, directoryData]);
-  useEffect(() => {
-    if (locationSearch) {
-      const lowerSearch = locationSearch.toLowerCase();
-      const results = directoryData.filter((item) =>
-        [item.city, item.organization.location]
-          .filter(Boolean) // removes null/undefined
-          .some((field) => field.toLowerCase().includes(lowerSearch))
-      );
-      setFilteredResults(results);
-    } else {
-      setFilteredResults(directoryData); // if no search, show all
-    }
-  }, [locationSearch, directoryData]);
->>>>>>> 5a9c440e2f60e0b1db4f4ebf1e29f002e9fd7a2c
 
   useEffect(() => {
     const getAllProfessionals = async () => {
